53052390175cbaeb5f6b9fc681c44c5ab734ec68
Mickael Lavroff <mickael.lavroff@gmail.com>
2021-12-17 16:12:20 -0500

    Add software base list

---
+++ Install-BaseSoftware.ps1
@@ -0,0 +1,35 @@
+# Base software
+winget install --id=Microsoft.PowerToys -e -h
+winget install --id=Microsoft.Office -e -h 
+winget install --id=7zip.7zip -e -h
+winget install --id=VideoLAN.VLC -e -h
+winget install --id=File-New-Project.EarTrumpet  -e -h
+
+# Scripting tools
+winget install --id=Microsoft.Git -e -h
+winget install --id=Microsoft.PowerShell -e -h
+winget install --id=Microsoft.WindowsTerminal -e -h
+winget install --id=JanDeDobbeleer.OhMyPosh -e -h
+winget install --id=Microsoft.VisualStudioCode -e -h
+
+# Azure related tools
+winget install --id=Microsoft.AzureCLI -e -h
+Winget install --id=Microsoft.Bicep -e -h
+winget install --id=Microsoft.AzureStorageExplorer -e -h
+winget install --id=Microsoft.AzureDataStudio -e -h
+winget install --id=Microsoft.AzureFunctionsCoreTools -e -h
+
+# Work related software
+winget install --id=Microsoft.Teams -e -h
+winget install --id=Microsoft.PowerBI -e -h
+winget install --id=ShiningLight.OpenSSL -e -h
+
+# Optional utilities
+winget install --id=NickeManarin.ScreenToGif -e -h
+winget install --id=DominikReichl.KeePass -e -h 
+winget install --id=OBSProject.OBSStudio  -e -h
+
+# Game related software
+winget install --id=Valve.Steam -e -h
+winget install --id=EpicGames.EpicGamesLauncher -e -h
+winget install --id=Discord.Discord -e -h
